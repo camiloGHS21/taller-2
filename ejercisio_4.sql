1d689d2a3178efda346b1b6f9011e5e23644a9e5
camiloghs21 <136854384+camiloGHS21@users.noreply.github.com>
2024-05-02 12:39:39 -0700

    Update ejercisio_4.sql
    actlizacion de ejercisio

--- ejercisio_4.sql
+++ ejercisio_4.sql
@@ -1,17 +1,11 @@
 --4 - Mostrar por pantalla al estudiante con el ID mayor y al estudiante con el ID menor.
-
+SET SERVEROUTPUT ON;
 DECLARE
   v_id_es_menor estudiantes.id%TYPE;
   v_id_es_mayor estudiantes.id%TYPE;
-  v_nombre_menor estudiantes.nombre%TYPE;
-  v_nombre_mayor estudiantes.nombre%TYPE;
 BEGIN
-  SELECT id, nombre INTO v_id_es_menor, v_nombre_menor
-  FROM estudiantes
-  WHERE id = (SELECT MIN(id) FROM estudiantes);
-  SELECT id, nombre INTO v_id_es_mayor, v_nombre_mayor
-  FROM estudiantes
-  WHERE id = (SELECT MAX(id) FROM estudiantes);
-  DBMS_OUTPUT.PUT_LINE('Estudiante con el ID menor: ' || v_id_es_menor || ', Nombre: ' || v_nombre_menor);
-  DBMS_OUTPUT.PUT_LINE('Estudiante con el ID mayor: ' || v_id_es_mayor || ', Nombre: ' || v_nombre_mayor);
+  SELECT MAX(id),MIN(id) INTO v_id_es_menor,v_id_es_mayor from estudiantes;
+  DBMS_OUTPUT.PUT_LINE('Estudiante con el ID menor: ' || v_id_es_menor );
+  DBMS_OUTPUT.PUT_LINE('Estudiante con el ID mayor: ' || v_id_es_mayor );
 END;
+
